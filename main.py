9f587dafb1d60bf68187c5ffe15f847e49383670
misha <kevlishvilimisha@yahoo.com>
2017-06-03 08:43:41 +0400

    recv all and local proxy listener

---
+++ main.py
@@ -0,0 +1,77 @@
+import socket
+import settings
+
+import threading
+import time
+
+
+
+
+def recv_all(conn):
+    
+    result=b''
+    while True:
+        try:
+                conn.settimeout(settings.socket_timeout)
+                data=conn.recv(4096 )
+                conn.settimeout(None)
+                if data:result+=data
+                else:
+                    raise Exception('sock_closed')
+        except socket.timeout:
+            conn.settimeout(None)
+             
+            print("received all")
+            
+    return result
+
+
+
+
+class local_server():
+    
+    #შიდა პროქსის ip და port რომელიც უნდა მიუთითოთ ბრაუზერში
+    local_proxy_ip='127.0.0.1'
+    local_proxy_port=1327
+    
+    def start_server(self):
+        
+        
+        # იხსნება სოკეტი და იწყება პორტზე მოსმენა
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((self.local_proxy_ip, self.local_proxy_port))
+        sock.listen(5)
+        
+        while True:
+            conn,addr=sock.accept()
+            thr = threading.Thread(target=self.request_handler, args=(conn, addr))
+            thr.start()
+            
+    def request_handler(self,conn,addr):
+        
+        
+        try:
+            #მივიღოთ დატა ბრაუზერისგან,ან სხვა პროქსი კლიენტისგან
+            request=recv_all(conn)
+            
+            
+            
+            
+            
+            
+            
+        except:
+            pass
+            
+        finally:
+            conn.close()
+            
+            
+            
+            
+            
+            
+            
+        
+    
+    
