85fef0b962923826b5b5c15750a5fe0ec9fedbb4
misha <kevlishvilimisha@yahoo.com>
2017-09-06 19:25:09 +0400

    performance improvement

--- main.py
+++ main.py
@@ -67,60 +67,7 @@ class local_server():
             thr = threading.Thread(target=self.request_handler, args=(conn, addr))
             thr.start()
     
-    # def request_fragment_geocell_sender(self, request: str):
-    #
-    #     fragment_array = re.findall(''.join('(\S{{{}}})'.format(4000)), request)
-    #
-    #     request_id = self.get_next_request_count()
-    #
-    #     for fragment in fragment_array:
-    #
-    #         data_to_send = json.dumps({'op': 'send', 'request_id': request_id, 'data': fragment}, ensure_ascii=False)
-    #
-    #         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #
-    #         server_address = (settings.remote_server_ip, settings.remote_server_port)
-    #
-    #         counter = 0
-    #         status=''
-    #         while counter < settings.max_resend_try:
-    #
-    #             counter=counter+1
-    #             sock.connect(server_address)
-    #
-    #             try:
-    #                 sock.settimeout(4)
-    #                 sock.sendall(data_to_send)
-    #                 sock.settimeout(None)
-    #
-    #                 sock.settimeout(4)
-    #
-    #
-    #                 ack = sock.recv(4000)
-    #                 sock.settimeout(None)
-    #
-    #                 if ack:
-    #                     sock.close()
-    #                     status=request_id
-    #
-    #
-    #                     break
-    #                 else:
-    #                     sock.settimeout(None)
-    #                     sock.close()
-    #                     status=False
-    #                     continue
-    #
-    #             except socket.timeout:
-    #                 sock.settimeout(None)
-    #                 sock.close()
-    #                 status=False
-    #                 continue
-    #
-    #         if not status:
-    #             return status
-    #
-    #     return status
+    
 
     def geocell_sender(self, request,request_id=None):
     
@@ -339,12 +286,12 @@ class local_server():
                     self.geocell_receiver(request_id, https=True)
                    
                     while True:
-                      
+                        data = b''
                         request=b''
                         while True:
                         
                             try:
-                                 conn.settimeout(0.2)
+                                 conn.settimeout(0.1)
                                  t=conn.recv(65000)
                                  conn.settimeout(None)
                                  request+=t
