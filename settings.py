d2454e499b72314c99cce3fb4bb497c55650182d
misha <kevlishvilimisha@yahoo.com>
2017-09-08 18:27:59 +0400

    performance improvement

--- settings.py
+++ settings.py
@@ -2,10 +2,11 @@ socket_timeout=2#წამი
 
 #'188.226.160.247'
 #139.162.149.221
-remote_server_ip='188.226.160.247' #digit
+# remote_server_ip='188.226.160.247' #digit
 # remote_server_ip='139.162.149.221' #linode
+remote_server_ip='46.101.207.138' #digit fra
 remote_server_port=1327
-max_resend_try=5
-global_timeout=0.8
+max_resend_try=3
+global_timeout=0.5
 responce_timeout=4
-max_fragment_size=65000
+max_fragment_size=40000
