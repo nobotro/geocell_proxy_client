4eb4f2befdc25a339cd3110992aea58e4dd12da0
nobotro <kevlishvilimisha@gmail.com>
2017-12-19 20:03:40 +0400

    add logs

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 #'188.226.160.247'
 #139.162.149.221
 # remote_server_ip='188.226.160.247' #digit
-remote_server_ip='127.0.0.1' #linode
+remote_server_ip='139.162.149.221' #linode
 # remote_server_ip='46.101.207.138' #digit fra
 remote_server_port_range=range(1328,1500)
 max_resend_try=7
